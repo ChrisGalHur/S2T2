ea0870e976d2521648e035241c65c4ba3eb79cd4
ChrisGalHur <108464294+ChrisGalHur@users.noreply.github.com>
2022-11-22 19:47:47 +0100

    Update S2T2.sql

--- S2T2.sql
+++ S2T2.sql
@@ -163,10 +163,17 @@ WHERE profesor.id_departamento IS NULL;
 
 -- Consultes resum:
 -- Retorna el nombre total d'alumnes que hi ha.
-SELECT COUNT(persona.id_persona) FROM persona
+SELECT COUNT(persona.id) FROM persona
 WHERE tipo = 'alumno';
 -- Calcula quants alumnes van néixer en 1999.
+SELECT COUNT(persona.id) FROM persona
+WHERE tipo = 'alumno' 
+AND fecha_nacimiento BETWEEN '1999-01-01' AND '1999-12-31';
 -- Calcula quants professors/es hi ha en cada departament. El resultat només ha de mostrar dues columnes, una amb el nom del departament i una altra amb el nombre de professors/es que hi ha en aquest departament. El resultat només ha d'incloure els departaments que tenen professors/es associats i haurà d'estar ordenat de major a menor pel nombre de professors/es.
+SELECT count(profesor.id_profesor), departamento.nombre FROM departamento 
+INNER JOIN profesor ON departamento.id = profesor.id_departamento 
+GROUP BY profesor.id_departamento 
+ORDER BY count(profesor.id_profesor) DESC, departamento.nombre; 
 -- Retorna un llistat amb tots els departaments i el nombre de professors/es que hi ha en cadascun d'ells. Tingui en compte que poden existir departaments que no tenen professors/es associats. Aquests departaments també han d'aparèixer en el llistat.
 -- 5-Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun. Tingues en compte que poden existir graus que no tenen assignatures associades. Aquests graus també han d'aparèixer en el llistat. El resultat haurà d'estar ordenat de major a menor pel nombre d'assignatures.
 -- Retorna un llistat amb el nom de tots els graus existents en la base de dades i el nombre d'assignatures que té cadascun, dels graus que tinguin més de 40 assignatures associades.
@@ -174,5 +181,4 @@ WHERE tipo = 'alumno';
 -- Retorna un llistat que mostri quants alumnes s'han matriculat d'alguna assignatura en cadascun dels cursos escolars. El resultat haurà de mostrar dues columnes, una columna amb l'any d'inici del curs escolar i una altra amb el nombre d'alumnes matriculats.
 -- Retorna un llistat amb el nombre d'assignatures que imparteix cada professor/a. El llistat ha de tenir en compte aquells professors/es que no imparteixen cap assignatura. El resultat mostrarà cinc columnes: id, nom, primer cognom, segon cognom i nombre d'assignatures. El resultat estarà ordenat de major a menor pel nombre d'assignatures.
 -- 10-Retorna totes les dades de l'alumne/a més jove.
-
 -- Retorna un llistat amb els professors/es que tenen un departament associat i que no imparteixen cap assignatura.
